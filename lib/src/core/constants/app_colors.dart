a7745e1aee35c40ccd8a50f5978eb265365f1443
petrotorskyy <x3mweb@gmail.com>
2023-01-20 15:44:26 +0200

    Add smooth_page_indicator

--- lib/src/core/constants/app_colors.dart
+++ lib/src/core/constants/app_colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   //Splash Screen
   static const Color textLogoColor = Color(0xFFDA7200);
+
+  //Onboarding
+  static const Color onBoardingBg = Color(0xFFFF4B3A);
 }
