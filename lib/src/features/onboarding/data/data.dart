24c3ecbfff36ccfa4a3af28e720e888e29800b7b
petrotorskyy <x3mweb@gmail.com>
2023-02-25 19:30:37 +0200

    Delete list of subtitles

--- lib/src/features/onboarding/data/data.dart
+++ lib/src/features/onboarding/data/data.dart
@@ -7,13 +7,6 @@ final List<String> titlesList = [
   'Fast, rescued food at your service.',
 ];
 
-final List<String> subtitlesList = [
-  '',
-  '',
-  '',
-  'Unlock the world of regular and rescued food by setting up your delivery address.',
-];
-
 final List<dynamic> imageList = [
   AppPng.imageFirst,
   AppPng.imageSecond,
